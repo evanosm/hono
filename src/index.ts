2d6e8f90408c818f2de232c94babd4fddca44199
Evan OSMONT <evan.osmont@gmail.com>
2023-02-02 20:24:51 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { serveStatic } from "hono/serve-static.bun";
 
 // routes
@@ -8,15 +9,26 @@ import projects from "./routes/projects.route";
 // constants
 const port = parseInt(process.env.PORT) || 3000;
 const app = new Hono();
-
+//CORS
+app.use('/projects/*', cors({
+	origin: '*',
+	allowMethods: ['GET'],
+}))
 
 app.use("*", async (c, next) => {
 	if (c.req.param("id")) {
-    console.log(`${c.req.method} ${c.req.url}| (at ${new Date().toLocaleString()}))`);
-    console.log(`🪄 - See on website at: https://www.vean.fr/projects/${c.req.param("id")}`)
-
-  } else {
-		console.log(`${c.req.method} ${c.req.url} | (at ${new Date().toLocaleString()}))`);
+		console.log(
+			`${c.req.method} ${c.req.url}| (at ${new Date().toLocaleString()}))`,
+		);
+		console.log(
+			`🪄 - See on website at: https://www.vean.fr/projects/${c.req.param(
+				"id",
+			)}`,
+		);
+	} else {
+		console.log(
+			`${c.req.method} ${c.req.url} | (at ${new Date().toLocaleString()}))`,
+		);
 	}
 	await next();
 });
